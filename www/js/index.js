d0fac0d145788dc1b5b191f90dd32992cc4d55cc
Idan Tsadok <idan.ts@gmail.com>
2015-06-22 23:06:54 +0300

    added GPS test removed focus to hours input on init

--- www/js/index.js
+++ www/js/index.js
@@ -194,8 +194,6 @@ var app = {
                 }
             }
         }
-
-        $('#hours').focus();
     },
     // Bind Event Listeners
     //
@@ -210,6 +208,26 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
         app.receivedEvent('deviceready');
+
+        var onSuccess = function(position) {
+            alert('Latitude: '          + position.coords.latitude          + '\n' +
+                'Longitude: '         + position.coords.longitude         + '\n' +
+                'Altitude: '          + position.coords.altitude          + '\n' +
+                'Accuracy: '          + position.coords.accuracy          + '\n' +
+                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+                'Heading: '           + position.coords.heading           + '\n' +
+                'Speed: '             + position.coords.speed             + '\n' +
+                'Timestamp: '         + position.timestamp                + '\n');
+        };
+
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
         document.addEventListener("backbutton", function(){
             if (!exitConfirmOpened) {

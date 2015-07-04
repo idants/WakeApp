4bd5d146292af8fb09a3074fd68b51c51acc3433
Idan Tsadok <idan.ts@gmail.com>
2015-07-04 17:58:32 +0300

    logs

--- www/js/index.js
+++ www/js/index.js
@@ -262,7 +262,6 @@ var app = {
                 endPosition = element.selectionEnd,
                 firstChar = e.target.value.charAt(0);
 
-            //alert('start:' + startPosition + ' end:' + endPosition);
             if (e.keyCode === 8) { //backspace
                 if (startPosition === endPosition && startPosition !== 0) {
                     if (firstChar === '0') {
@@ -307,6 +306,7 @@ var app = {
                     e.preventDefault();
                 }
             }
+            alert('start:' + startPosition + ' end:' + endPosition);
         } catch (e) {
             alert('checkDel error: ' + e.message);
         }
@@ -340,7 +340,7 @@ var app = {
                 limit = isHours ? 23 : 59,
                 newValueNum = -1;
 
-            //alert('start:' + startPosition + ' end:' + endPosition + ' newVal:' + newValueText);
+            alert('start:' + startPosition + ' end:' + endPosition + ' newVal:' + newValueText);
             // case of entering a digit before a 0# pattern
             if (newValueText.length === 3 && newValueText.charAt(0) !== '0' && valueBeforeTyping.charAt(0) === '0') {
                 newValueText = newValueText.substring(0, 1) + newValueText.substring(2); // remove the '0' in the middle
